c071093921dd4f58cb9bf0102942a4d3c1b9a5eb
Stanislav Sartasov <Stanislav.Sartasov@gmail.com>
2015-11-23 10:52:47 +0400

    - Added the final task and updated the list of questions

---
+++ A_Rubanik/Autumn/ProducerConsumer/ProducerConsumer/Producer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ProducerConsumer
+{
+    class Producer : Program
+    {
+        Random MyRandom;
+            public Producer(int n)
+            {
+                MyRandom = new Random(n);
+            }
+            public void Producing()
+            {
+                while(true)
+                {
+                    if (Console.KeyAvailable)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        int AddingNumber = MyRandom.Next(100);
+                        mutex.WaitOne();
+                        list.Add(AddingNumber);
+                        mutex.ReleaseMutex();
+                        Console.WriteLine("I add to list " + AddingNumber);
+                        int timeSleep = 100 + MyRandom.Next(1000);
+
+                        Thread.Sleep(timeSleep);
+                    }
+                }
+            }      
+    
+    }
+}
---
+++ A_Rubanik/Autumn/ThreadPool/ThreadPool/MyThreadPool.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadPool
+{
+    class MyThreadPool : IDisposable
+    {
+        delegate void FunctionOfEndWork();
+        event FunctionOfEndWork FinishingWork;
+        int NumberOfThreads;
+        List<MyThread> Threads;
+
+        public MyThreadPool(int n)
+        {
+            NumberOfThreads = n;
+            Threads = new List<MyThread>();
+            for (int i = 0; i < n; i++)
+            {
+                Threads.Add(new MyThread(i));
+                FinishingWork += Threads[i].EndWork;
+            }
+        }
+
+        public void Add(Action a)
+        {
+            int NumberOfMethodsInFirstThread = Threads[0].QueueOfMethods.Count;
+            int MinIndex = 0;
+            foreach (MyThread n in Threads)
+            {
+                n.LockerOfQueue.WaitOne();
+                int NumberOfMethodsInCurrentThread = n.QueueOfMethods.Count;
+                n.LockerOfQueue.ReleaseMutex();
+                if (NumberOfMethodsInCurrentThread < NumberOfMethodsInFirstThread)
+                {
+                    NumberOfMethodsInFirstThread = n.QueueOfMethods.Count;
+                    MinIndex = Threads.IndexOf(n);
+                }
+            }
+            Threads[MinIndex].LockerOfQueue.WaitOne();
+            Threads[MinIndex].Enqueue(a);
+            Threads[MinIndex].LockerOfQueue.ReleaseMutex();
+        }
+        public void Dispose()
+        {
+            FinishingWork();
+        }
+    }
+}
