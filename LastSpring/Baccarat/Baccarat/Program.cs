150c65f256c24f57e01c4c2d9a50a929e06ab621
Nikita <nikit121a@mail.ru>
2016-08-22 00:03:02 +0300

    LastSpring problem

---
+++ LastSpring/Baccarat/Baccarat/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace Baccarat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start money = 1000 ye");
+            Console.WriteLine("Rate = 50 ye");
+            Console.WriteLine("Time: 40 games");
+            Console.WriteLine();
+
+            Console.Write("Enter Strategy (1 - all rates for lose player,\n 2 - all rates for win player, 3 - all rates for drow): ");
+            while (1 == 1)
+            {
+                int str = Convert.ToInt32(Console.ReadLine());
+                GameLogic gm = new GameLogic(str);
+                gm.Game();
+                Console.WriteLine("Result game: {0} ye", gm.resultGame);
+                Console.WriteLine();
+                Console.Write("For continue enter Strategy:");
+            }
+        }
+    }
+    
+}
