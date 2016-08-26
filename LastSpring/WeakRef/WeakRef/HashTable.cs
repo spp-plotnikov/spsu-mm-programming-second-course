1dcf11d1bfe2e6b074e3ab1f31be312e05c249ef
Nikita <nikit121a@mail.ru>
2016-08-26 13:46:28 +0300

    New problem: WeakRef

---
+++ LastSpring/WeakRef/WeakRef/HashTable.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WeakRef
+{
+    class HashTable<T>
+    {
+        internal int StrongRefTime;
+        private int variable;
+        const int tableSize = 3;
+        List<WeakReference>[] hashTable;
+        List<object> strongRef;
+
+        public HashTable(int time)
+        {
+            variable = 0;
+            StrongRefTime = time;
+            hashTable = new List<WeakReference>[tableSize];
+            for (int i = 0; i < 3; i++)
+            {
+                hashTable[i] = new List<WeakReference>();
+            }
+            strongRef = new List<object>();
+        }
+
+        public void Add(T data)
+        {
+            variable++;
+
+            object obj = data;
+
+            int hash = obj.GetHashCode() % tableSize;
+            WeakReference weakRef = new WeakReference(obj);
+            hashTable[hash].Add(weakRef);
+
+            strongRef.Add(obj);
+
+            if (hash % 2 == 0)
+            {
+                TimerCallback timerCallBack = new TimerCallback(RemoveStrongRef);
+                Timer timer = new Timer(timerCallBack, variable, StrongRefTime, Timeout.Infinite);
+            }
+        }
+
+        public void Find(T obj)
+        {
+            int hash = obj.GetHashCode() % tableSize;
+            foreach (WeakReference weakRef in hashTable[hash])
+            {
+                if (weakRef.IsAlive)
+                    if (weakRef.Target.Equals(obj))
+                    {
+                        Console.WriteLine(obj.ToString() + " Found");
+                        return;
+                    }
+            }
+            Console.WriteLine(obj.ToString() + " Not found");
+        }
+
+        public void Delete(T obj)
+        {
+            int hash = obj.GetHashCode() % tableSize;
+            foreach (WeakReference weakRef in hashTable[hash])
+            {
+                if (weakRef.IsAlive)
+                    if (weakRef.Target.Equals(obj))
+                    {
+                        weakRef.Target = null;
+                        return;
+                    }
+            }
+            Console.WriteLine(obj.ToString() + " Not found");
+        }
+        
+        private void RemoveStrongRef(object variable)
+        {
+            int pos = (int)variable;
+            strongRef[pos - 1] = null;
+        }
+    }
+}
---
+++ LastSpring/WeakRef/WeakRef/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WeakRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int StrongRefTime = 1000;
+            var hashTab = new HashTable<int>(StrongRefTime);
+            for (int i = 1; i < 100; i++)
+            {
+                hashTab.Add(i);
+            }
+
+            Thread.Sleep(StrongRefTime + 50);
+            GC.Collect();
+
+            for (int i = 1; i < 100; i++)
+            {
+                hashTab.Find(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
