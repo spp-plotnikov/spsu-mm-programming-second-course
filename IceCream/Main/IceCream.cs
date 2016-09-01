5c80b1809506110aa6f5e8533df8a1de9a802072
Nadezhda Petelimova <hikari.aka.nadya@yandex.ru>
2016-09-01 13:57:36 +0300

    IceCream
    CodeStyle

---
+++ IceCream/Main/IceCream.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace icecream
+{
+    class IceCream
+    {
+        public abstract class icecream
+        {
+            public string packaging;
+            public string type;
+            public string baseingredient;
+            public string toping;
+            public string form;
+            public double kkal;
+            public string sprinklingConfectionery;
+
+            public abstract void recipe();
+
+        }
+
+        public class greentea : icecream
+        {
+            public greentea()
+            {
+                packaging = "bowl";
+                baseingredient = "green tea";
+                type = "Ice cream from green tea";
+                toping = "";
+                form = "ball";
+                kkal = 346;
+                sprinklingConfectionery = "";
+            }
+            public override void recipe()
+            {
+                Console.WriteLine(type + "\n");
+                Console.Write("The milk is heated, preventing boiling. Lower in hot milk " + baseingredient +
+                    "Cover and infuse.Pour into a container and freezeю Two hours later put in" + packaging + "in the form of" + form);
+                if (toping != "") Console.WriteLine("Add" + toping);
+                if (sprinklingConfectionery != "") Console.WriteLine("Add" + sprinklingConfectionery);
+                Console.WriteLine("\n This will be " + kkal + " kkal");
+            } // реализация абстрактного метода
+        }
+
+        public class pistachio : icecream
+        {
+            public pistachio()
+            {
+                packaging = "waffle cone";
+                baseingredient = "crushed pistachio";
+                type = "Pistachio ice cream with almonds";
+                toping = "almonds";
+                form = "soft";
+                kkal = 359;
+                sprinklingConfectionery = "";
+            }
+            public override void recipe()
+            {
+                Console.WriteLine(type + "\n");
+                Console.Write("The milk is heated, preventing boiling. Lower in hot milk " + baseingredient +
+                    "Cover and infuse.Pour into a container and freezeю Two hours later put in" + packaging + "in the form of" + form);
+                if (toping != "") Console.WriteLine("Add" + toping);
+                if (sprinklingConfectionery != "") Console.WriteLine("Add" + sprinklingConfectionery);
+                Console.WriteLine("\n This will be " + kkal + " kkal");
+            } // реализация абстрактного метода
+        }
+    }
+}
---
+++ IceCream/Main/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace icecreamprog
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            icecream.IceCream.greentea ice1 = new icecream.IceCream.greentea();
+            icecream.IceCream.pistachio ice2 = new icecream.IceCream.pistachio();
+
+            ice1.recipe();
+            ice2.recipe(); 
+        }
+    }
+}
