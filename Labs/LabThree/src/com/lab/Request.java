ba22834bd11a4681ce0438eaad53039eee03c0d2
Tytysha <evilina_19@mail.ru>
2016-09-11 14:49:48 +0500

    Manufacturer - Consumer

---
+++ Labs/LabThree/src/com/lab/Request.java
@@ -0,0 +1,19 @@
+package com.lab;
+
+/**
+ * Created by Katrin on 15.06.2016.
+ */
+public class Request {
+
+    private String number;
+
+    public Request(String number) {
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    // Some request..
+}
