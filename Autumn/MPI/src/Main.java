c52bed44e8ffcc9b5debefd7f6c1733fc97dfaeb
chugaev <chugaev.tolya@gmail.com>
2016-10-31 20:53:34 +0300

    рабочее решение

--- Autumn/MPI/src/Main.java
+++ Autumn/MPI/src/Main.java
@@ -2,28 +2,36 @@ import mpi.MPI;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.*;
 import java.lang.Integer;
-import java.util.Vector;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public final static int root = 0;
+    public final static int INF = Integer.MAX_VALUE / 2;
     public static void main(String[] args) throws IOException{
-//        long startTime = System.currentTimeMillis();
+        long startTime = System.currentTimeMillis();
         MPI.Init(args);
-        File inputFile = null, outputFile = null;
-        int me = MPI.COMM_WORLD.Rank();
+        //File inputFile = null,
+		File outputFile = null;
+		//FileInputStream fileInputStream = null;        
+		int me = MPI.COMM_WORLD.Rank();
         int size = MPI.COMM_WORLD.Size();
-        Scanner sc = null;
+        //Scanner sc = null;
+		BufferedReader bi = null;
         PrintWriter pw = null;
-        int n,m = 0;
+        int n = 0;
         if (me == root) {
-            inputFile = new File(args[3]);
+            //inputFile = new File(args[3]);
+			//fileInputStream = new FileInputStream(args[3]);
             outputFile = new File(args[4]);
-            sc = new Scanner(inputFile);
+            //sc = new Scanner(inputFile); 						//долгое считывает
+			bi = new BufferedReader(new FileReader(args[3])); 	//так быстрее
             pw = new PrintWriter(outputFile);
-            n = sc.nextInt();
-            int[] buf = {n};
+            //n = sc.nextInt();
+			n = Integer.parseInt(bi.readLine());            
+			int[] buf = {n};
             for (int i = 1; i < MPI.COMM_WORLD.Size(); i++) {
                 MPI.COMM_WORLD.Send(buf, 0, 1, MPI.INT, i, 99);
             }
@@ -32,104 +40,108 @@ public class Main {
             MPI.COMM_WORLD.Recv(buf, 0, 1, MPI.INT, root, 99);
             n = buf[0];
         }
-        Vector<Integer> g[] = new Vector[n + 1];
-        Vector<Integer> weight[] = new Vector[n + 1];
-        boolean[] used = new boolean[n + 1];
+        int[][] graph = new int[n][n];
+        boolean[] used = new boolean[n];
+        int[] dist = new int[n];
+        Arrays.fill(dist, INF);
+        dist[0] = 0;
         if (me == root) {
-            for (int i = 1; i <= n; i++) {
-                g[i] = new Vector<Integer>();
-                weight[i] = new Vector<Integer>();
-            }
-            while (sc.hasNext()) {
+            /*while (sc.hasNext()) {		
                 int a = sc.nextInt();
                 int b = sc.nextInt();
                 int c = sc.nextInt();
-                g[a].add(b);
-                g[b].add(a);
-                weight[a].add(c);
-                weight[b].add(c);
-            }
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j < size; j++) {
-                    int[] buf1 = new int[]{g[i].size()};
-                    MPI.COMM_WORLD.Send(buf1, 0, 1, MPI.INT, j, 4);
-                    int[] buf2 = new int[g[i].size()];
-                    for (int k = 0; k < g[i].size(); k++) {
-                        buf2[k] = g[i].get(k);
-                    }
-                    MPI.COMM_WORLD.Send(buf2, 0, buf2.length, MPI.INT, j, 5);
-                    for (int k = 0; k < g[i].size(); k++) {
-                        buf2[k] = weight[i].elementAt(k);
-                    }
-                    MPI.COMM_WORLD.Send(buf2, 0, buf2.length, MPI.INT, j, 6);
-                }
-            }
+                graph[a][b] = graph[b][a] = c;
+            }*/
+			String line;
+			while ((line = bi.readLine()) != null) {
+				int[] mas = new int[3];
+				int i = 0;
+    			for (String numStr: line.split("\\s")) {
+        			mas[i++] = Integer.parseInt(numStr);
+				}
+				int a = mas[0];
+				int b = mas[1];
+				int 	c = mas[2];
+				graph[a][b] = graph[b][a] = c;
+			}	
 
+			for (int i = 0; i < n; i++) {
+                int[] buf = new int[n];
+                for (int j = 0; j < n; j++) {
+                    buf[j] = graph[i][j];
+                }
+                for (int j = 1; j < size; j++) {	
+                    MPI.COMM_WORLD.Send(buf, 0, buf.length, MPI.INT, j, 10);
+			    }
+            }
+			//System.out.println("adadada");
         } else {
-            for (int i = 1; i <= n; i++) {
-                int[] buf1 = new int[1];
-                MPI.COMM_WORLD.Recv(buf1, 0, 1, MPI.INT, root, 4);
-                int len = buf1[0];
-                int[] buf2 = new int[len];
-                MPI.COMM_WORLD.Recv(buf2, 0, len, MPI.INT, root, 5);
-                g[i] = new Vector<Integer> ();
-                for (int j = 0; j < len; j++) {
-                    g[i].add(buf2[j]);
-                }
-                weight[i] = new Vector<Integer>();
-                MPI.COMM_WORLD.Recv(buf2, 0, len, MPI.INT, root, 6);
-                for (int j = 0; j < len; j++) {
-                    weight[i].add(buf2[j]);
-                }
+			for (int i = 0 ; i < n; i++) {
+                int[] buf = new int[n];
+                MPI.COMM_WORLD.Recv(buf, 0, n, MPI.INT, root, 10);
+                for (int k = 0; k < n; k++) {
+                	graph[i][k] = buf[k];
+            	}
             }
         }
-        used[1] = true;
-        long sum = 0;
-        for (int k = 0; k < n - 1; k++) {
-            int min_weight = Integer.MAX_VALUE;
-            int to = 0;
-            for (int i = me; i < g.length; i += size) {
-                if (used[i]) {
-                    for (int j = 0; j < g[i].size(); j++) {
-                        if (weight[i].elementAt(j) < min_weight && !used[g[i].elementAt(j)]) {
-                            min_weight = weight[i].get(j);
-                            to = g[i].get(j);
+	//System.out.println(me);
+        // закончили пересылку входных  данных
+		if (me == root) {
+            System.out.println((System.currentTimeMillis() - startTime) / 1000.0);
+        }
+        for (int i = 0; i < n; i++) {
+			//System.out.println(n + " " + size + " " + me);
+            int v = -1;
+            for (int nv = me; nv < n; nv += size) {
+                if (!used[nv] && dist[nv] < INF && (v == -1 || dist[v] > dist[nv])) {
+                    v = nv;
+                }
+            }
+            if (me == root) {
+                for (int j = 1; j < MPI.COMM_WORLD.Size(); j++) {
+                    int[] buf = new int[1];
+                    MPI.COMM_WORLD.Recv(buf, 0 , 1, MPI.INT, j, 11);
+                    int vv = buf[0];
+                    if (v == -1) {
+                        v = vv;
+                    } else if (vv != -1 && dist[v] > dist[vv]) {
+                        v = vv;
+                    }
+                }
+                int[] buf = new int[]{v};
+                for (int j = 1; j < MPI.COMM_WORLD.Size(); j++) {
+                    MPI.COMM_WORLD.Send(buf, 0, buf.length, MPI.INT, j, 12);
+                }
+            } else {
+                int[] buf  = new int[]{v};
+                MPI.COMM_WORLD.Send(buf, 0, buf.length, MPI.INT, root, 11);
+                MPI.COMM_WORLD.Recv(buf, 0, 1, MPI.INT, root, 12);
+                v = buf[0];
+            }
+            if (v != -1) {
+                used[v] = true;
+                for (int nv = 0; nv < n; nv++) {
+                    if (!used[nv] && graph[v][nv] != 0) {
+                        if (dist[nv] > graph[v][nv]) {
+                            dist[nv] = graph[v][nv];
                         }
                     }
                 }
             }
-            if(me == root) {
-                for (int i = 1; i < size; i++) {
-                    int[] buf = new int[2];
-                    MPI.COMM_WORLD.Recv(buf, 0 , 2, MPI.INT, i, i);
-                    if (min_weight > buf[1]) {
-                        min_weight = buf[1];
-                        to = buf[0];
-                    }
-                }
-            } else {
-                int[] buf = new int[]{to, min_weight};
-                MPI.COMM_WORLD.Send(buf, 0, buf.length, MPI.INT, root, me);
-            }
-            if (me == root) {
-                used[to] = true;
-                sum += min_weight;
-                int[] buf = new int[]{to};
-                for (int i = 1; i < size; i++) {
-                    MPI.COMM_WORLD.Send(buf, 0, 1, MPI.INT, i, 20);
-                }
-            } else {
-                int[] buf = new int[1];
-                MPI.COMM_WORLD.Recv(buf, 0, 1, MPI.INT, root, 20);
-                to = buf[0];
-                used[to] = true;
-            }
         }
         if (me == root) {
+            int sum = 0;
+            //System.out.println(Arrays.toString(dist));
+            for (int i = 0; i < n; i++) {
+                sum += dist[i];
+            }
             pw.println(n);
             pw.println(sum);
             pw.close();
-//            System.out.println((System.currentTimeMillis() - startTime) / 1000.0);
+            System.out.println((System.currentTimeMillis() - startTime) / 1000.0);
         }
+	//System.out.println(me);
+	MPI.Finalize();
     }
 }
+
