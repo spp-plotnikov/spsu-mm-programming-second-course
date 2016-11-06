19ef5e253daecfd7f4f21d6cbba35994b58e4f82
Volkov Grigoriy <grihanaft@mail.ru>
2016-11-06 15:26:24 +0300

    Add files via upload

---
+++ Autumn/Common/SemaphoreList/ListWSemaphore.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+public class ListWSemaphore<T> : List<T>
+{
+    private static int maxNumOfThreads = 2; // Number of maximum concurrent threads
+    public int curNumOfThreads = 0; // Number of current threads, working with list
+    private List<T> list = new List<T>(); // Just list
+
+    public int CurNumOfThreads // Property
+    {
+        get
+        {
+            return curNumOfThreads;
+        }
+    }
+
+    public void Add(T item) // Adding an item
+    {
+        while (maxNumOfThreads <= curNumOfThreads) { Console.WriteLine("{0} Waiting, curThreads = {1}", Thread.CurrentThread.Name, curNumOfThreads); Thread.Sleep(100); }
+        curNumOfThreads++;
+        list.Add(item);
+        curNumOfThreads--;
+    }
+
+    public void RemoveAt(int index) // Removing an item by index
+    {
+        while (maxNumOfThreads <= curNumOfThreads) { Console.WriteLine("{0} Waiting, curThreads = {1}", Thread.CurrentThread.Name, curNumOfThreads); Thread.Sleep(100); }
+        curNumOfThreads++;
+        try
+        {
+            list.RemoveAt(index);
+        }
+        catch { }
+        curNumOfThreads--;
+    }
+
+    public void LockFrvr() // Making object inaccessible to changes
+    {
+        curNumOfThreads = -1;
+    }
+
+    public void WriteList() // WriteLine list
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            Console.Write(list[i]);
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Autumn/Common/SemaphoreList/ProducerNConsumer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+public class Producer
+{
+    public static void StartCycle<T>(ListWSemaphore<T> list, T elem) // The first param is list and the second is object to add
+    {
+        while (list.CurNumOfThreads >= 0) // While list is available to changes do something
+        {
+            list.Add(elem);
+            Console.WriteLine("{0} did something", Thread.CurrentThread.Name);
+            Thread.Sleep(200);
+        }
+    }
+}
+
+public class Consumer
+{
+    public static void StartCycle<T>(ListWSemaphore<T> list, int index) // The first param is list and the second is object index to remove
+    {
+        while (list.CurNumOfThreads >= 0) // While list is available to changes do something
+        {
+            list.RemoveAt(index);
+            Console.WriteLine("{0} did something", Thread.CurrentThread.Name); 
+            Thread.Sleep(200);
+        }
+    }
+}
