09e24ff6f3eab92872a032e8f0fe5c3ad9b3419f
Vladimir Miloserdov <milosvova@gmail.com>
2016-11-07 14:28:27 +0300

    Сделал ThreadPool

---
+++ Autumn/Common/ThreadPool/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        MyThreadPool pool = new MyThreadPool();
+        pool.start();
+        long st = System.currentTimeMillis();
+        for (int i = 0; i < 10; i++) {
+            Runnable task = () -> {
+                int me = ((int) Thread.currentThread().getId());// % MyThreadPool.threadMax + 1);
+                try {
+                    System.out.println("[" + (System.currentTimeMillis() - st) + "] Thread " + me + ": started");
+                    Thread.sleep(ThreadLocalRandom.current().nextInt(100, 5000));
+                    System.out.println("[" + (System.currentTimeMillis() - st) + "] Thread " + me + ": finished");
+                }
+                catch (InterruptedException e) {
+                    System.out.println("[" + (System.currentTimeMillis() - st) + "] Thread " + me + ": terminated");
+                }
+            };
+            pool.enqueue(task);
+            // for the sake of emulation...
+            Thread.sleep(ThreadLocalRandom.current().nextInt(200, 400));
+        }
+        Thread.sleep(5000); // give 'em some time
+        pool.doStop();
+        pool.join();
+    }
+}
---
+++ Autumn/Common/ThreadPool/src/MyThreadPool.java
@@ -0,0 +1,62 @@
+import java.util.Iterator;
+import java.util.List;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class MyThreadPool extends Thread {
+    public static int threadMax = 2;
+    private Queue<Runnable> pending;
+    private List<Thread> running;
+    public volatile boolean isStoped = true;
+
+    public MyThreadPool() {
+        running = new LinkedList<>();
+        pending = new LinkedList<>();
+    }
+
+    public void enqueue(Runnable obj) {
+        synchronized (pending) {
+            pending.add(obj);
+        }
+    }
+
+    public void run() {
+        isStoped = false;
+        // control loop
+        while (!isStoped) {
+            // check if any of running threads is done
+            Iterator<Thread> it = running.iterator();
+            while (it.hasNext()) {
+                Thread cur = it.next();
+                if (!cur.isAlive())
+                    it.remove();
+            }
+
+            // check whether it is possible to start some new threads
+            synchronized (pending) {
+                while (!pending.isEmpty() && running.size() < threadMax) {
+                    running.add(new Thread(pending.poll()));
+                    running.get(running.size() - 1).start();
+                }
+            }
+        }
+    }
+
+    public void doStop() {
+        isStoped = true;
+        for (Thread cur : running) {
+            cur.interrupt();
+            try {
+                cur.join();
+            }
+            catch (InterruptedException e) {
+                // who cares?
+            }
+        }
+    }
+git 
+    @Override
+    protected void finalize() {
+        this.doStop();
+    }
+}
