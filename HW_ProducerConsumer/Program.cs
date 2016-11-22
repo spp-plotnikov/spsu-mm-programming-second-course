01adf6fe1f872ce471ba60cc4e4d7d0ba4f2a9b0
cmirnov <cmirnov239kirill@gmail.com>
2016-11-22 16:12:46 +0300

    fixed some critical bugs

--- HW_ProducerConsumer/Program.cs
+++ HW_ProducerConsumer/Program.cs
@@ -14,7 +14,8 @@ namespace ProducerConsumer
         public const int numOfCons = 3;
         public const int Wait = 1000;
 
-        public static int isLocked = 0; //0 - free
+        public static int IsLocked = 0; //0 - free
+        public static bool IsEnd = false;
 
         public static List<int> SharedList = new List<int>();
         public static List<Thread> CurrentProducer = new List<Thread>();
@@ -23,6 +24,7 @@ namespace ProducerConsumer
         static void Main(string[] args)
         {
             Console.WriteLine("press any key for end:");
+            
 
             for (int i = 0; i < numOfProd; i++)
             {
@@ -41,18 +43,18 @@ namespace ProducerConsumer
             }
             Console.ReadKey();
 
-            while (0 != Interlocked.CompareExchange(ref isLocked, 1, 0))
+            while (0 != Interlocked.CompareExchange(ref IsLocked, 1, 0))
             { }
 
             Console.WriteLine("that's all");
 
+            IsEnd = true;
 
             for (int i = 0; i < numOfCons; i++)
             {
                 CurrentConsumer[0].Join();
                 CurrentConsumer.RemoveAt(0);
             }
-
             for (int i = 0; i < numOfProd; i++)
             {  
                 CurrentProducer[0].Join();
@@ -63,61 +65,63 @@ namespace ProducerConsumer
         }
 
 
-        public class Producer
+    }
+
+    class Producer
+    {
+        int name;
+
+        public Producer(int name)
         {
-            int name;
-
-            public Producer(int name)
-            {
-                this.name = name;
-            }
-
-            public void Run()
-            {
-                while (true)
-                {
-                    while (0 != Interlocked.CompareExchange(ref isLocked, 1, 0))
-                    { }
-                    Random rand = new Random();
-                    int val = rand.Next(100);
-                    SharedList.Add(val);
-                    Console.WriteLine("Producer # {0} add number {1} to list", name, val);
-                    Interlocked.Exchange(ref isLocked, 0);
-                    Thread.Sleep(Wait);
-                }
-            }
-
+            this.name = name;
         }
 
-        class Consumer
+        public void Run()
         {
-            int name;
-
-            public Consumer(int name)
+            while (!Program.IsEnd)
             {
-                this.name = name;
+                while (0 != Interlocked.CompareExchange(ref Program.IsLocked, 1, 0))
+                { }
+                Random rand = new Random();
+                int val = rand.Next(100);
+                Program.SharedList.Add(val);
+                Console.WriteLine("Producer # {0} add number {1} to list", name, val);
+                Interlocked.Exchange(ref Program.IsLocked, 0);
+                Thread.Sleep(Program.Wait);
             }
+        }
 
-            public void Run()
+    }
+
+    class Consumer
+    {
+        int name;
+
+        public Consumer(int name)
+        {
+            this.name = name;
+        }
+
+        public void Run()
+        {
+            while (!Program.IsEnd)
             {
-                while (true)
+                while (0 != Interlocked.CompareExchange(ref Program.IsLocked, 1, 0))
+                { }
+                if (Program.SharedList.Count == 0)
                 {
-                    while (0 != Interlocked.CompareExchange(ref isLocked, 1, 0))
-                    { }
-                    if (SharedList.Count == 0)
-                    {
-                        Console.WriteLine("Consumer # {0} can't get number because list is empty", name);
-                    }
-                    else
-                    {
-                        int val = SharedList.Last();
-                        Console.WriteLine("Consumer # {0} get {1} from list", name, val);
-                        SharedList.Remove(val);
-                    }
-                    Interlocked.Exchange(ref isLocked, 0);
-                    Thread.Sleep(Wait);
+                    Console.WriteLine("Consumer # {0} can't get number because list is empty", name);
                 }
+                else
+                {
+                    int val = Program.SharedList.Last();
+                    Console.WriteLine("Consumer # {0} get {1} from list", name, val);
+                    Program.SharedList.Remove(val);
+                }
+                Interlocked.Exchange(ref Program.IsLocked, 0);
+                Thread.Sleep(Program.Wait);
             }
         }
     }
+
 }
