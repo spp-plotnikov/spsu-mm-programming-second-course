c25810feeca873f0607c30ca531081143f231985
Kanteev Leonid <lkolt2@mail.ru>
2016-11-23 15:51:09 +0300

    Version 1.0
    Реализован основной функционал

---
+++ Autumn/Common/Home tasks/3. Mutex/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Producer_consumer
+{
+    class Mutexes
+    {
+        public static int Delay = 500;
+        public static bool IsWorking = true;
+        public static Mutex Mtx = new Mutex();
+        public static List<int> Buf = new List<int>();
+    }
+
+    class Producer
+    {
+        private int name;
+        public Producer(int num)
+        {
+            name = num;
+        }
+
+        public void Run()
+        {
+            while (Mutexes.IsWorking)
+            {
+                Mutexes.Mtx.WaitOne();
+                Console.WriteLine("Producer " + name + " has bocked mutex");
+                Mutexes.Buf.Add(name);
+                Console.WriteLine("Producer " + name + " add " + name + " at top of buffer");
+                Mutexes.Mtx.ReleaseMutex();
+                Console.WriteLine("Producer " + name + " has released mutex");
+                Thread.Sleep(Mutexes.Delay);
+            }
+        }
+    }
+
+    class Consumer
+    {
+        private int name;
+        public Consumer(int num)
+        {
+            name = num;
+        }
+
+        public void Run()
+        {
+            while (Mutexes.IsWorking)
+            {
+                while (Mutexes.Buf.Count < 1)
+                {
+                    Thread.Sleep(Mutexes.Delay);
+                }
+                Mutexes.Mtx.WaitOne();
+                Console.WriteLine("Consumer " + name + " has bocked mutex");
+                int drop = Mutexes.Buf.First();
+                Mutexes.Buf.Remove(drop);
+                Console.WriteLine("Consumer " + name + " drop " + drop + " at begin of buffer");
+                Mutexes.Mtx.ReleaseMutex();
+                Console.WriteLine("Consumer " + name + " has released mutex");
+                Thread.Sleep(Mutexes.Delay);
+            }
+        }
+    }
+
+    class Program
+    {
+        public static List<Thread> ConsumerList = new List<Thread>();
+        public static List<Thread> ProducerList = new List<Thread>();
+        public static int NumConsumers = 3;
+        public static int NumProducers = 3;
+
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= NumProducers; i++)
+            {
+                Producer newProducer = new Producer(i);
+                Thread newThread = new Thread(() => newProducer.Run());
+                ProducerList.Add(newThread);
+                newThread.Start();
+            }
+            for (int i = 1; i <= NumConsumers; i++)
+            {
+                Consumer newConsumer = new Consumer(i);
+                Thread newThread = new Thread(() => newConsumer.Run());
+                ConsumerList.Add(newThread);
+                newThread.Start();
+            }
+            Console.ReadKey();
+            Console.WriteLine("\nStopping all threads...");
+            Mutexes.IsWorking = false;
+            for (int i = 1; i <= NumProducers; i++)
+            {
+                ProducerList[0].Join();
+                ProducerList.RemoveAt(0);
+            }
+            for (int i = 1; i <= NumConsumers; i++)
+            {
+                ConsumerList[0].Join();
+                ConsumerList.RemoveAt(0);
+            }
+            Console.WriteLine("All process was stopped");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Autumn/Common/Home tasks/3. Mutex/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Producer-consumer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Producer-consumer")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("07515ed2-aa9f-4e3e-af9d-b872c7bd870a")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
