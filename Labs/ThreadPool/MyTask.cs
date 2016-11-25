97b667bd2171cf7edf30c8090da6940b7d5a2832
Irina Len <ia.len@mail.ru>
2016-11-25 10:46:22 +0300

    MyTask corrected

--- Labs/ThreadPool/MyTask.cs
+++ Labs/ThreadPool/MyTask.cs
@@ -10,15 +10,17 @@ namespace ThreadPool
     class MyTask
     {
         string name;
+        int num = 1;
 
         public MyTask(int n)
         {
             name = "Task " + n;
+            num = n;
         }
            
         public void StartWork()
         {
-            int r = new Random().Next(1000, 10000);
+            int r = new Random().Next(1000, 10000) * (num + 1) / 3;
             Console.WriteLine("{0} start, work {1}", name, r);
             Thread.Sleep(r);
         }
