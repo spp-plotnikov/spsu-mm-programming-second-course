5839f9147eef1061c51b828aaa1fdabe5673673a
Irina Len <ia.len@mail.ru>
2016-11-28 23:04:36 +0300

    Change random

--- Labs/FibersManager/ProcessManagerFramework.cs
+++ Labs/FibersManager/ProcessManagerFramework.cs
@@ -107,7 +107,7 @@ namespace FibersManager
             {
                 Thread.Sleep(1);
                 int rand = new Random().Next(0, fibers.Count() - 1);
-                if ((rand / fibers.Count) * 100.0 > 7)
+                if ((rand / (float)fibers.Count) * 100.0 < 7)
                 {
                     fibNow = rand;
                 }
