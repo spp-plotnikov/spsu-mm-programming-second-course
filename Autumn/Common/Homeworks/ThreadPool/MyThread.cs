f967a7ccd70361378f09c076c197b95dc828e7af
Your Name <sabrina.musatian@yandex.ru>
2016-11-28 17:14:11 +0300

    new class

---
+++ Autumn/Common/Homeworks/ThreadPool/MyThread.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyThreadPool
+{
+    class MyThread
+    {
+        private Thread _mySubThread;
+        private bool _work = true;
+        private ManualResetEvent _working = new ManualResetEvent(false);
+        private Queue<Action> _poolActions = new Queue<Action>();
+
+        public MyThread(Queue<Action> poolActions)
+        {
+            _poolActions = poolActions;
+            _mySubThread = new Thread(MyThreadStart);
+            _mySubThread.Start();
+        }
+
+        public bool Busy
+        {
+            get
+            {
+                return _working.WaitOne(0);
+            }
+        }
+
+        public void GiveSignal()
+        {
+            _working.Set();
+        }
+
+        public void MyThreadStart()
+        {
+            while (_work)
+            {
+                // prevent from other threads changes of the tasks queue
+                Monitor.Enter(_poolActions);
+
+                // if there is something to do
+                if (_poolActions.Count > 0)
+                {
+                    Action myCurAction = _poolActions.Dequeue();
+                    myCurAction();
+                    Console.WriteLine("The task is completed");
+                    Console.WriteLine();
+                    Console.WriteLine("---------------------------------------------------");
+                    Monitor.Exit(_poolActions);
+                }
+                else
+                {
+                    // don't need to change the tasks queue, finish blocking immediately
+                    Monitor.Exit(_poolActions);
+
+                    // waiting for some new job or to finish the thread
+                    _working.Reset();
+                    _working.WaitOne();
+                }
+            }
+        }
+
+        // end the thread
+        public void Finish()
+        {
+            _work = false;
+            _working.Set();
+            _mySubThread.Join();
+        }
+    }
+}
--- Autumn/Common/Homeworks/ThreadPool/ThreadPool.cs
+++ Autumn/Common/Homeworks/ThreadPool/ThreadPool.cs
@@ -9,22 +9,22 @@ namespace MyThreadPool
 {
     public class ThreadPool : IDisposable
     {
-        int _numOfThreads = 5;
-        MyThread[] _threads = new MyThread[5];
-        public static Queue<Action> PoolActions = new Queue<Action>();
+        private int _numOfThreads = 5;
+        private MyThread[] _threads = new MyThread[5];
+        private Queue<Action> _poolActions = new Queue<Action>();
 
         public ThreadPool()
         {
             for (int i = 0; i < _numOfThreads; i++)
             {
-                _threads[i] = new MyThread();
+                _threads[i] = new MyThread(_poolActions);
             }
         }
 
         public void AddToPool(Action newAction)
         {
-            Monitor.Enter(PoolActions);
-            PoolActions.Enqueue(newAction);
+            Monitor.Enter(_poolActions);
+            _poolActions.Enqueue(newAction);
             int itr = 0;
 
             // find the thread to do the job
@@ -38,7 +38,7 @@ namespace MyThreadPool
                 _threads[itr].GiveSignal();
             }
 
-            Monitor.Exit(PoolActions);
+            Monitor.Exit(_poolActions);
         }
 
         public void Dispose()
@@ -50,72 +50,6 @@ namespace MyThreadPool
 
             Console.WriteLine("All threads deleted");
         }
-
-        private class MyThread
-        {
-            private Thread _mySubThread;
-            private bool _work = true;
-            private ManualResetEvent _finished = new ManualResetEvent(false);
-            private ManualResetEvent _working = new ManualResetEvent(false);
-
-            public MyThread()
-            {
-                _mySubThread = new Thread(MyThreadStart);
-                _mySubThread.Start();
-            }
-
-            public bool Busy
-            {
-                get
-                {
-                    return _working.WaitOne(0);
-                }
-            }
-
-            public void GiveSignal()
-            {
-                _working.Set();
-            }
-
-            public void MyThreadStart()
-            {
-                while (_work)
-                {
-                    // prevent from other threads changes of the tasks queue
-                    Monitor.Enter(PoolActions);
-
-                    // if there is something to do
-                    if (PoolActions.Count > 0)
-                    {
-                        Action myCurAction = PoolActions.Dequeue();
-                        myCurAction();
-                        Console.WriteLine("The task is completed");
-                        Console.WriteLine();
-                        Console.WriteLine("---------------------------------------------------");
-                        Monitor.Exit(PoolActions);
-                    }
-                    else
-                    {
-                        // don't need to change the tasks queue, finish blocking immediately
-                        Monitor.Exit(PoolActions);
-
-                        // waiting for some new job or to finish the thread
-                        _working.Reset();
-                        _finished.Set();
-                        _working.WaitOne();
-                        _finished.Reset();
-                    }
-                }
-            }
-
-            // end the thread
-            public void Finish()
-            {
-                _work = false;
-                _finished.WaitOne();
-                _working.Set();
-                _mySubThread.Join();
-            }
-        }
+       
     }
 }
