535e77312c373239494f4db0bca1f5f352ea90ff
Petr Shumilov <pilot-shumilov@yandex.ru>
2016-12-02 17:26:10 +0600

    Дописал matrix

--- Autumn/Common/1.MPI/matrix.cs
+++ Autumn/Common/1.MPI/matrix.cs
@@ -3,17 +3,16 @@ using System.IO;
 
 class Matrix
 {
-    private int[,] matrix;
+    public int[] matrix;
     public int Size;
 
     public const int INF = 100000000;
-    
-    public Matrix(string path, int q)
+
+    public Matrix(int[] matrix, int Size)
     {
-        StreamReader fileHandler = new StreamReader(path);
-
-        this.Size = Int32.Parse(fileHandler.ReadLine());
-
+        this.Size = Size;
+        this.matrix = new int[Size * Size];        
+        this.matrix = matrix;
     }
 
     public Matrix(string path)
@@ -22,7 +21,7 @@ class Matrix
 
         this.Size = Int32.Parse(fileHandler.ReadLine());
 
-        this.matrix = new int[this.Size, this.Size];
+        this.matrix = new int[this.Size * this.Size];
 
         this.init(INF, 0);
 
@@ -33,65 +32,42 @@ class Matrix
             int i = Int32.Parse(param[0]);
             int j = Int32.Parse(param[1]);
             int w = Int32.Parse(param[2]);
-            this.matrix[i, j] = w;
-            this.matrix[j, i] = w;
-
+            this.matrix[i * this.Size + j] = w;
+            this.matrix[j * this.Size + i] = w;
         }
         fileHandler.Close();
     }
 
     private void init(int cell, int diag)
     {
-        for (int h = 0; h < this.Size; ++h)
-        {
-            for (int w = 0; w < this.Size; ++w)
-            {
-                if (h == w)
-                    this.matrix[h, w] = diag;
-                else
-                    this.matrix[h, w] = cell;
-            }
-        }
+        for (int i = 0; i < this.Size * this.Size; i++)
+            this.matrix[i] = cell;        
+        for (int i = 0; i < this.Size; i++)
+            this.matrix[i * this.Size + i] = diag;
     }
 
     public void print(string path = "")
     {
         if (path != "")
         {
-            StreamWriter outputFile = new StreamWriter(path);
-            for (int i = 0; i < this.Size; ++i)
+            System.IO.StreamWriter file = new System.IO.StreamWriter(path);
+            for (int i = 0; i < this.Size; i++)
             {
-                for (int j = 0; j < this.Size; ++j)
-                    outputFile.Write(this.matrix[i, j] + " ");
-                outputFile.WriteLine();
+                for (int j = 0; j < this.Size; j++)
+                {
+                    file.Write(this.matrix[i * this.Size + j] + " ");
+                }
+                file.WriteLine();
             }
-            outputFile.Close();
+            file.Close();
             return;
         }
         for (int i = 0; i < this.Size; ++i)
         {
             for (int j = 0; j < this.Size; ++j)
-                Console.Write(this.matrix[i, j] + " ");
+                Console.Write(this.matrix[i * this.Size + j] + " ");
             Console.WriteLine();
         }
-    }
-
-    public int[] getRow(int num)
-    {
-        int[] row = new int[this.Size];
-        for (int j = 0; j < this.Size; ++j)
-            row[j] = this.matrix[num, j];
-        return row;
-    }
-
-    public int getCell(int i, int j)
-    {
-        return this.matrix[i, j];
-    }
-
-    public void updateCell(int i, int j, int value)
-    {
-        this.matrix[i, j] = value;
     }
 }
 
