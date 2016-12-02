aa23855769b5f2b4d2be75525a2be573c09d1911
Petr Shumilov <pilot-shumilov@yandex.ru>
2016-12-03 03:56:38 +0600

    ThreadPool done

--- Autumn/Common/4.ThreadPool/Program.cs
+++ Autumn/Common/4.ThreadPool/Program.cs
@@ -5,107 +5,28 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 
-class Worker
-{
-    private bool Runneble = false;
-    private Thread Thread;
-    private Queue<Action> TasksQueue;
-
-    public Worker(Queue<Action> tasksQueue)
-    {
-        this.Thread = new Thread(Run);
-        this.TasksQueue = tasksQueue;
-    }
-
-    public void Start()
-    {
-        this.Thread.Start();
-        this.Runneble = true;
-    }
-
-    public void Stop()
-    {
-        this.Runneble = false;
-    }
-
-    public void Run()
-    {
-        // worker loop
-        while(Runneble)
-        {
-            Action curTask = new Action(() => { });
-            lock(TasksQueue)
-            {
-                // if queue not empty
-                if (TasksQueue.Count() != 0)
-                {
-                    curTask = TasksQueue.Dequeue();
-                    Console.WriteLine("I'm " + Thread.CurrentThread.Name + " is working now");
-                    Monitor.Pulse(TasksQueue);
-                }
-                else
-                {
-                    Console.WriteLine("Waiting...");
-                    Monitor.Wait(TasksQueue);
-                }
-            }
-            if (!Runneble)
-                return;
-            curTask();
-        }
-    }
-}
-
-
 namespace ThreadPool
 {
     class Program
     {
-        public static void MyTask()
-        {
-            Console.WriteLine("executing");
-            Thread.Sleep(300);
-        }
-
         static void Main(string[] args)
         {
-            Queue<Action> tasksQueue = new Queue<Action>();
-           
+            // init pool
+            ThreadPool pool = new ThreadPool();
 
-            Worker th1 = new Worker(tasksQueue);
-            th1.Start();
-            Worker th2 = new Worker(tasksQueue);
-            th2.Start();
+            // example of task
+            Action task = new Action(() => {
+                Console.WriteLine("Doing something...");
+                Thread.Sleep(500);
+            });
 
-            Worker th3 = new Worker(tasksQueue);
-            th3.Start();
+            // push to pool
+            for (int i = 0; i < 30; ++i)
+                pool.Enqueue(task);
 
-            Action task = new Action(MyTask);
+            // press key before disposing pool
             Console.ReadKey();
-            lock (tasksQueue)
-            {
-                Monitor.Pulse(tasksQueue);
-            }
-            tasksQueue.Enqueue(task);
-
-            //tasksQueue.Enqueue(task);
-            //tasksQueue.Enqueue(task);
-
-            Console.WriteLine("Done!");
-            Console.ReadKey();
-
-            lock (tasksQueue)
-            {
-                th1.Stop();
-                th2.Stop();
-                th3.Stop();
-                Monitor.PulseAll(tasksQueue);
-                Console.WriteLine("all threads have been stopped");
-            }
-
-            //Monitor.PulseAll(tasksQueue);
-            //th1.Stop();
-
+            pool.Dispose();
         }
     }
 }
---
+++ Autumn/Common/4.ThreadPool/ThreadPool.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace ThreadPool
+{
+    class ThreadPool : IDisposable
+    {
+        const int workersNum = 5;
+        private Queue<Action> tasksQueue = new Queue<Action>();
+        private List<Worker> workers = new List<Worker>();
+
+        public ThreadPool()
+        {
+            for (int i = 0; i < workersNum; ++i)
+            {
+                Worker worker = new Worker(tasksQueue);
+                workers.Add(worker);
+                worker.Start();
+            }
+        }
+
+        public void Enqueue(Action task)
+        {
+            lock (tasksQueue)
+            {
+                // push task
+                tasksQueue.Enqueue(task);
+                // push to threads queue
+                Monitor.Pulse(tasksQueue);
+            }
+        }
+
+        public void Dispose()
+        {
+            lock (tasksQueue)
+            {
+                tasksQueue.Clear();
+                for (int i = 0; i < workersNum; ++i)
+                    workers[i].Stop();
+
+                Monitor.PulseAll(tasksQueue);
+            }
+        }
+    }
+}
