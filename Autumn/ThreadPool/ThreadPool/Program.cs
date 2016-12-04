4ed2127b2ae3bd930c19dd4953edef322e1f9948
avlomakin <Alom2222@gmail.com>
2016-12-04 12:23:14 +0300

    ThreadPool v0.9

---
+++ Autumn/ThreadPool/ThreadPool/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var threadPool = new ThreadPool())
+            {
+                Action action = () =>
+                {
+                    Thread.Sleep(400);
+                    Console.WriteLine(Thread.CurrentThread.ManagedThreadId + " thread finished the task!");
+                };
+
+                threadPool.Start();
+
+                var isFinished = (object)(false);
+
+                Thread thr = new Thread(() =>
+                {
+                    while (!(bool)isFinished)
+                    {
+                        Thread.Sleep(200);
+                        threadPool.Enqueue(action);
+                    }
+                });
+
+                thr.Start();
+  
+                Console.ReadLine();
+                isFinished = true;
+            }
+        }
+    }
+}
---
+++ Autumn/ThreadPool/ThreadPool/ThreadPool.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ThreadPool
+{
+    public class ThreadPool : IDisposable
+    {
+        private readonly int _threadCount = 20;
+        private List<Thread> _threads;
+        private Queue<Action> _tasks; 
+        private object _syncObj = new object();
+        private bool _isFinished;
+         
+
+        public ThreadPool()
+        {
+            _threads = new List<Thread>(_threadCount);
+            for (int i = 0; i < _threadCount; i++)
+            {
+                _threads.Add(new Thread(Work));
+            }
+            _tasks = new Queue<Action>();
+            _isFinished = false;
+        }
+
+        public void Start()
+        {
+            foreach (var thread in _threads)
+            {
+                thread.Start();
+            }
+        }
+
+        private void Work()
+        {
+            while (!_isFinished)
+            {
+                Action task;
+                lock (_syncObj)
+                {
+
+                    while (_tasks.Count == 0 && !_isFinished)
+                    {
+                        Monitor.Wait(_syncObj);
+                    }
+
+                    if (_isFinished)
+                        return;
+
+                    task = _tasks.Dequeue();
+                }
+                task.Invoke();
+            }
+        }
+
+        public void Enqueue(Action action)
+        {
+            lock (_syncObj)
+            {
+                _tasks.Enqueue(action);
+                Monitor.PulseAll(_syncObj);
+            }
+        }
+
+        public void Dispose()
+        {
+            lock (_syncObj)
+            {
+                _isFinished = true;
+                Monitor.PulseAll(_syncObj);
+            }
+        }
+    }
+}
