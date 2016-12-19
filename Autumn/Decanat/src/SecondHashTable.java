cd8c4f7641735facd5ee987d259f99eabf66ca68
chugaev <chugaev.tolya@gmail.com>
2016-12-19 23:03:13 +0300

    newfix

--- Autumn/Decanat/src/SecondHashTable.java
+++ Autumn/Decanat/src/SecondHashTable.java
@@ -1,8 +1,8 @@
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.ArrayList;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class SecondHashTable implements IExamSystem {
-    private RefinableHashTable<Long, CopyOnWriteArrayList> hashMap;
+    private RefinableHashTable<Long, ArrayList> hashMap;
     private int countStudents;
     ReentrantLock lock;
 
@@ -14,28 +14,34 @@ public class SecondHashTable implements IExamSystem {
 
     public void add(long studentId, long courseId) {
         lock.lock();
-        CopyOnWriteArrayList<Long> list = hashMap.get(studentId);
+        ArrayList<Long> list = hashMap.get(studentId);
         if (list == null) {
-            list = new CopyOnWriteArrayList();
+            list = new ArrayList();
             list.add(courseId);
             hashMap.put(studentId, list);
         }
         lock.unlock();
-        if (!list.contains(courseId)) {
-            list.add(courseId);
+        synchronized (list) {
+            if (!list.contains(courseId)) {
+                list.add(courseId);
+            }
         }
     }
 
     public void remove(long studentId, long courseId) {
-        CopyOnWriteArrayList<Long> list = hashMap.get(studentId);
-        list.remove(courseId);
+        ArrayList<Long> list = hashMap.get(studentId);
+        synchronized (list) {
+            list.remove(courseId);
+        }
     }
 
     public boolean contains(long studentId, long courseId) {
-        CopyOnWriteArrayList<Long> list = hashMap.get(studentId);
+        ArrayList<Long> list = hashMap.get(studentId);
         if (list == null) {
             return false;
         }
-        return  list.contains(courseId);
+        synchronized (list) {
+            return list.contains(courseId);
+        }
     }
 }
