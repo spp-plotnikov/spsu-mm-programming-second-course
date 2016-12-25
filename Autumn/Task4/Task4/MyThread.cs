684e781dfd5010439c88e0f7a7c6861b3fea30b6
OffemsivePlant31 <Fast.n.plant31@gmail.com>
2016-12-25 18:37:46 +0300

    ThreadPool v1.0

---
+++ Autumn/Task4/Task4/MyThread.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    public class MyThread
+    {
+        private Queue<Action> tasks;
+        private bool isWorking;
+        private Thread thread;
+        private String name;
+        public int TaskCounter
+        {
+            get;
+            private set;
+        }
+        public delegate void MyDelegate();
+        public event MyDelegate IsReady;
+
+        public MyThread(String name)
+        {
+            //this.tasks = tasks;
+            thread = new Thread(Run);
+            this.name = name;
+            TaskCounter = 0;
+        }
+
+        public void Enqueue(Action task)
+        {
+            tasks.Enqueue(task);
+            TaskCounter++;
+        }
+
+        public void Run()
+        {
+            while(isWorking)
+            {
+                Action onGoing = new Action(() => { });
+                lock(tasks)
+                {
+                    if(tasks.Count() != 0)
+                    {
+                        onGoing = tasks.Dequeue();
+                        Console.WriteLine(name + "is running.");
+                    }
+                    else
+                    {
+                        Console.WriteLine(name + "is not busy. Trying to steal.");
+                        IsReady();
+                    }
+                }
+                if(!isWorking)
+                {
+                    return;
+                }
+                onGoing();
+                TaskCounter--;
+            }
+        }
+
+        public Action Give()
+        {
+            lock(tasks)
+            {
+                isWorking = false;
+                try
+                {
+                    Action task = tasks.Dequeue();
+                    TaskCounter--;
+                    isWorking = true;
+                    Monitor.PulseAll(tasks);
+                    return task;
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        public void Start()
+        {
+            thread.Start();
+            isWorking = true;
+        }
+        public void Stop()
+        {
+            isWorking = false;
+        }
+        public void Dispose()
+        {
+            lock(tasks)
+            {
+                Stop();
+                tasks.Clear();
+                TaskCounter = 0;
+                Monitor.PulseAll(tasks);
+            }
+            return;
+        }
+    }
+}
---
+++ Autumn/Task4/Task4/ThreadPool.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    public class ThreadPool : IDisposable
+    {
+        private Queue<Action> tasks;
+        private int threadNumber;
+        private List<MyThread> threads;
+        public ThreadPool(Queue<Action> list, int number)
+        {
+            threadNumber = number;
+            tasks = list;
+            threads = new List<MyThread>();
+            for(int i = 0; i < threadNumber; i++)
+            {
+                MyThread thread = new MyThread(i.ToString());
+                threads.Add(thread);
+                thread.IsReady += StealTask;
+            }
+            while(tasks.Count != 0)
+            {
+                for (int i = 0; i < threadNumber; i++)
+                {
+                    threads[i].Enqueue(tasks.Dequeue());
+                }
+            }
+            for (int i = 0; i < threadNumber; i++)
+            {
+                threads[i].Start();
+            }
+        }
+
+        private void StealTask()
+        {
+            int max = threads[0].TaskCounter;
+            int maxIndex = 0;
+            for (int i = 0; i < threads.Count; i++)
+            {
+                if (threads[i].TaskCounter > max)
+                {
+                    max = threads[i].TaskCounter;
+                    maxIndex = i;
+                }
+            }
+            Enqueue(threads[maxIndex].Give());
+        }
+
+        public void Enqueue(Action task)
+        {
+            if(task == null)
+            {
+                return;
+            }
+            int min = threads[0].TaskCounter;
+            int minIndex = 0;
+            for(int i = 0; i < threads.Count; i++)
+            {
+                if(threads[i].TaskCounter < min)
+                {
+                    min = threads[i].TaskCounter;
+                    minIndex = i;
+                }
+            }
+            threads[minIndex].Enqueue(task);
+        }
+
+        public void Dispose()
+        {
+            for(int i = 0; i < threads.Count; i++)
+            {
+                threads[i].Dispose();
+            }
+        }
+    }
+}
