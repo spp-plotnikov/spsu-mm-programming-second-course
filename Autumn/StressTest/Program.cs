1f385abe7dd6986720ad60d45a3d1bc38bac1db1
Leonid Voroshilov <lewor1997@gmail.com>
2016-12-28 16:53:57 +0300

    lab 9 added

--- Autumn/StressTest/Program.cs
+++ Autumn/StressTest/Program.cs
@@ -12,40 +12,82 @@ namespace StressTest
     {
         static void Main(string[] args)
         {
-            int N = 3;
+            
+            string[] fileList = Directory.GetFiles(@"..\img", "*.bmp");
+
+            int N = fileList.Length;
+
+            int q = 3;
             long[] res = new long[N];
             List<Task<long>>[] tasks = new List<Task<long>>[N];
 
-            /* 1 ?! */
-
             /* 2 */
-            string[] fileList = Directory.GetFiles(@"..\img", "*.bmp");
-
+            
             for(int i = 0; i < N; i++)
-                tasks[i] = new List<Task<long>>();
+                for(int j  = 0; j < q; j++)
+                  tasks[i] = new List<Task<long>>();
 
-            foreach (var path in fileList)
-            {
-                for (int i = 0; i < N; i++)
-                    tasks[i].Add(new Task<long>(() => Emulation(path)));
-            }
+
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < q; j++)
+                {
+                    int index = i;
+                    tasks[i].Add(new Task<long>(() => Emulation(fileList[index])));
+                }
+           
             for (int i = 0; i < N; i++)
                 foreach (var t in tasks[i])
                     t.Start();
+
             long time = 0;
+            string res1 = "", res0 = "";
             for (int i = 0; i < N; i++)
             {
+                long[] f = new long[q];
                 time = 0;
+                int c = 0;
                 foreach (Task<long> task in tasks[i])
                 {
                     time += task.Result;
-                    Console.WriteLine(task.Result);
+                    f[c++] = task.Result;
+                    //Console.WriteLine(task.Result);
                 }
 
-                Console.WriteLine(">> {0}", time / N);
+                Console.WriteLine("{0} {1}",128 * Math.Pow(2, i), time / N);
+                Array.Sort(f);
+                res1 += ((128 * Math.Pow(2, i)).ToString() +" " +(time / N).ToString() + Environment.NewLine);
+                res0 += ((128 * Math.Pow(2, i)).ToString() + " " + (f[q / 2]).ToString() + Environment.NewLine);
             }
+            File.WriteAllText(@"0.txt", res0);
+            File.WriteAllText(@"1.txt", res1);
+
 
             /* 3 */
+            int count = 0;
+            string res2 = "";
+            while (true)
+            {
+                count++;
+                //Console.WriteLine("== {0} == ", count);
+
+                Task<long>[] hammer = new Task<long>[count];
+                for(int i = 0; i < count; i++)
+                    hammer[i] = (new Task<long>(() => Emulation(fileList[1])));
+
+                foreach (var t in hammer)
+                    t.Start();
+
+                long duration =  0;
+
+                foreach (var t in hammer)
+                    duration += t.Result;
+
+                Console.WriteLine("{0} {1}", count, duration);
+                res2 += (count.ToString() + " " +duration.ToString() + " "+ Environment.NewLine);
+                File.WriteAllText(@"2.txt", res2);
+
+
+            }
         }
 
         private static long Emulation(string path)
