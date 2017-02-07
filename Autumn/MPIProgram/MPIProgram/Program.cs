efb2d5eaa5673ebfadb27422d0b3809c1c4b2a55
Nikita <nikit121a@mail.ru>
2017-02-07 13:03:04 +0300

    Fix CodeStyle mistakes.

--- Autumn/MPIProgram/MPIProgram/Program.cs
+++ Autumn/MPIProgram/MPIProgram/Program.cs
@@ -15,34 +15,36 @@ namespace MPIProgram
             using (var env = new MPI.Environment(ref args))
             {
                 var world = Communicator.world;
-                int p = world.Size;             //процессора
-                int deg = DegreeTwo(p);
+                int proc = world.Size;             //процессора
+                int deg = considerDegreeTwo(proc);
                 int numberOfElement;            //число элементов
                 int primaryNumberOnEachProc;    //изначальное число элементов на один процессор
-                int bearEl = 0;                 //bearing element
-                var ByteNomber = new int[deg + 1]; //двоичное представление номера процессора
+                int bearingElem = 0;                
+                var ByteNumber = new int[deg + 1]; //двоичное представление номера процессора
 
                 string inputFileName = args[0];
                 string outputFileName = args[1];
                 var inputFile = new StreamReader(@inputFileName);
                 string read = inputFile.ReadLine();
                 var splitLine = read.Split(' ');
+
                 inputFile.Close();
+
                 numberOfElement = splitLine.Count();
                 int[] arr = new int[numberOfElement+1];
                 if (world.Rank == 0)
                     for (int i = 0; i < numberOfElement; i++)
                         arr[i] = int.Parse(splitLine[i]);
 
-                if (numberOfElement == numberOfElement / p * p)
-                    primaryNumberOnEachProc = numberOfElement / p;
+                if (numberOfElement == numberOfElement / proc * proc)
+                    primaryNumberOnEachProc = numberOfElement / proc;
                 else
-                    primaryNumberOnEachProc = numberOfElement / p + 1;
+                    primaryNumberOnEachProc = numberOfElement / proc + 1;
 
                 #region           //Участок кода на случай, когда число процессоров не N-мерный гиперкуб
                 if (world.Rank == 0)
                 {
-                    if (!testDegreeTwo(p))
+                    if (!testDegreeTwo(proc))
                     {
                         Console.WriteLine("\nЧисло процессоров не степень двойки, запустится непараллельный алгоритм\n" +
                             "Результат:");
@@ -52,117 +54,124 @@ namespace MPIProgram
                         Thread.Sleep(500);
                     }
                 }
-                if (!testDegreeTwo(p))
+                if (!testDegreeTwo(proc))
                     return;
                 #endregion
 
                 if (world.Rank == 0)
                 {
 
-                    for (int i = 0; i < p - 1; i++)                   //рассылка на р процессоров
+                    for (int i = 0; i < proc - 1; i++)                   //рассылка на р процессоров
                         for (int j = primaryNumberOnEachProc * i; j < primaryNumberOnEachProc * (i + 1); j++)     //участков массивов размером N
                             world.Send<int>(arr[j], i + 1, j - primaryNumberOnEachProc * i + 2);
                     
-                    int[] b = new int[numberOfElement];
-                    int bLenght = numberOfElement - primaryNumberOnEachProc * (p - 1);
-                    int bNewLenght = bLenght;
-                    for (int i = primaryNumberOnEachProc * (p - 1); i < numberOfElement; i++)
-                        b[i - primaryNumberOnEachProc * (p - 1)] = arr[i];
-                    qs(ref b, 0, numberOfElement - primaryNumberOnEachProc * (p - 1) - 1);
-                    bearEl = b[(numberOfElement - primaryNumberOnEachProc * (p - 1)) / 2];
-                    for (int i = 1; i <= p - 1; i++)      //Рассылка опорного элемента и кол-ва пересылаемых элементов
+                    int[] otherArr = new int[numberOfElement];
+                    int otherArrLenght = numberOfElement - primaryNumberOnEachProc * (proc - 1);
+                    int otherArrNewLenght = otherArrLenght;
+                    for (int i = primaryNumberOnEachProc * (proc - 1); i < numberOfElement; i++)
+                        otherArr[i - primaryNumberOnEachProc * (proc - 1)] = arr[i];
+                    qs(ref otherArr, 0, numberOfElement - primaryNumberOnEachProc * (proc - 1) - 1);
+                    bearingElem = otherArr[(numberOfElement - primaryNumberOnEachProc * (proc - 1)) / 2];
+                    for (int i = 1; i <= proc - 1; i++)      //Рассылка опорного элемента и кол-ва пересылаемых элементов
                     {
-                        world.Send<int>(bearEl, i, 0);
+                        world.Send<int>(bearingElem, i, 0);
                         world.Send<int>(primaryNumberOnEachProc, i, 1);
                     }
-                    solveByteNomber(ref ByteNomber, world.Rank);   //считаем двоичное представление номера процессора
+                    solveByteNomber(ref ByteNumber, world.Rank);   //считаем двоичное представление номера процессора
 
                     //At this moment we have: 1.ByteNomber[], 2.b[] и bLenght, 3.n, p, N, 4.deg
                     int index;
-                    int jumpToSecondProcessor = p;
+                    int jumpToSecondProcessor = proc;
                     for (int i = 0; i < deg; i++)
                     {
                         jumpToSecondProcessor /= 2;
 
                         if (i != 0)
-                            bearEl = world.Receive<int>(world.Rank + jumpToSecondProcessor, 0);
-                        index = findIndexBerEl(bearEl, b, bLenght);
-                        world.Send<int>(bLenght - index, world.Rank + jumpToSecondProcessor, 1);
-                        for (int j = index; j < bLenght; j++)
-                            world.Send<int>(b[j], world.Rank + jumpToSecondProcessor, j - index + 2);
-                        bLenght = index;
-                        bNewLenght = world.Receive<int>(world.Rank + jumpToSecondProcessor, 1);
-                        for (int j = 0; j < bNewLenght; j++)
-                            b[bLenght + j] = world.Receive<int>(world.Rank + jumpToSecondProcessor, j + 2);
-                        bLenght += bNewLenght;
-                        qs(ref b, 0, bLenght - 1);
+                            bearingElem = world.Receive<int>(world.Rank + jumpToSecondProcessor, 0);
+                        index = findIndexBerEl(bearingElem, otherArr, otherArrLenght);
+                        world.Send<int>(otherArrLenght - index, world.Rank + jumpToSecondProcessor, 1);
+                        for (int j = index; j < otherArrLenght; j++)
+                            world.Send<int>(otherArr[j], world.Rank + jumpToSecondProcessor, j - index + 2);
+                        otherArrLenght = index;
+                        otherArrNewLenght = world.Receive<int>(world.Rank + jumpToSecondProcessor, 1);
+                        for (int j = 0; j < otherArrNewLenght; j++)
+                            otherArr[otherArrLenght + j] = world.Receive<int>(world.Rank + jumpToSecondProcessor, j + 2);
+                        otherArrLenght += otherArrNewLenght;
+                        qs(ref otherArr, 0, otherArrLenght - 1);
                     }
+
                     var outputFile = new StreamWriter(@outputFileName, false);
                     outputFile.Write("world.Rank=" + world.Rank + ", b[]: ");
-                    for (int i = 0; i < bLenght; i++)
-                        outputFile.Write(b[i] + " ");
+                    for (int i = 0; i < otherArrLenght; i++)
+                        outputFile.Write(otherArr[i] + " ");
+
                     outputFile.Close();
-                    if (world.Rank != p - 1)
+
+                    if (world.Rank != proc - 1)
                         world.Send<int>(0, world.Rank + 1, 0);
                 }
                 else
                 {
-                    int[] b = new int[numberOfElement];
-                    int bLenght = world.Receive<int>(0, 1);
-                    int bNewLenght = bLenght;
-                    bearEl = world.Receive<int>(0, 0);
-                    for (int j = 0; j < bLenght; j++)
-                        b[j] = world.Receive<int>(0, j + 2);
-                    qs(ref b, 0, bLenght - 1);
-                    solveByteNomber(ref ByteNomber, world.Rank);   //считаем двоичное представление номера процессора
+                    int[] otherArr = new int[numberOfElement];
+                    int otherArrLenght = world.Receive<int>(0, 1);
+                    int otherArrNewLenght = otherArrLenght;
+                    bearingElem = world.Receive<int>(0, 0);
+                    for (int j = 0; j < otherArrLenght; j++)
+                        otherArr[j] = world.Receive<int>(0, j + 2);
+                    qs(ref otherArr, 0, otherArrLenght - 1);
+                    solveByteNomber(ref ByteNumber, world.Rank);   //считаем двоичное представление номера процессора
+
                     int index;
-                    int jumpToSecondProcessor = p;
+                    int jumpToSecondProcessor = proc;
                     for (int i = 0; i < deg; i++)
                     {
                         jumpToSecondProcessor /= 2;
-                        if (ByteNomber[deg - i] == 1)
+                        if (ByteNumber[deg - i] == 1)
                         {
                             if (i != 0)
                             {
-                                bearEl = b[bLenght / 2];
-                                world.Send<int>(bearEl, world.Rank - jumpToSecondProcessor, 0);
+                                bearingElem = otherArr[otherArrLenght / 2];
+                                world.Send<int>(bearingElem, world.Rank - jumpToSecondProcessor, 0);
                             }
-                            index = findIndexBerEl(bearEl, b, bLenght);
+                            index = findIndexBerEl(bearingElem, otherArr, otherArrLenght);
                             world.Send<int>(index, world.Rank - jumpToSecondProcessor, 1);
                             for (int j = index; j > 0; j--)
-                                world.Send<int>(b[index - j], world.Rank - jumpToSecondProcessor, index - j + 2);
-                            for (int j = index; j < bLenght; j++)
-                                b[j - index] = b[j];
-                            bLenght -= index;
-                            bNewLenght = world.Receive<int>(world.Rank - jumpToSecondProcessor, 1);
-                            for (int j = 0; j < bNewLenght; j++)
-                                b[bLenght + j] = world.Receive<int>(world.Rank - jumpToSecondProcessor, j + 2);
-                            bLenght += bNewLenght;
-                            qs(ref b, 0, bLenght - 1);
+                                world.Send<int>(otherArr[index - j], world.Rank - jumpToSecondProcessor, index - j + 2);
+                            for (int j = index; j < otherArrLenght; j++)
+                                otherArr[j - index] = otherArr[j];
+                            otherArrLenght -= index;
+                            otherArrNewLenght = world.Receive<int>(world.Rank - jumpToSecondProcessor, 1);
+                            for (int j = 0; j < otherArrNewLenght; j++)
+                                otherArr[otherArrLenght + j] = world.Receive<int>(world.Rank - jumpToSecondProcessor, j + 2);
+                            otherArrLenght += otherArrNewLenght;
+                            qs(ref otherArr, 0, otherArrLenght - 1);
                         }
                         else
                         {
                             if (i != 0)
-                                bearEl = world.Receive<int>(world.Rank + jumpToSecondProcessor, 0);
-                            index = findIndexBerEl(bearEl, b, bLenght);
-                            world.Send<int>(bLenght - index, world.Rank + jumpToSecondProcessor, 1);
-                            for (int j = index; j < bLenght; j++)
-                                world.Send<int>(b[j], world.Rank + jumpToSecondProcessor, j - index + 2);
-                            bLenght = index;
-                            bNewLenght = world.Receive<int>(world.Rank + jumpToSecondProcessor, 1);
-                            for (int j = 0; j < bNewLenght; j++)
-                                b[bLenght + j] = world.Receive<int>(world.Rank + jumpToSecondProcessor, j + 2);
-                            bLenght += bNewLenght;
-                            qs(ref b, 0, bLenght - 1);
+                                bearingElem = world.Receive<int>(world.Rank + jumpToSecondProcessor, 0);
+                            index = findIndexBerEl(bearingElem, otherArr, otherArrLenght);
+                            world.Send<int>(otherArrLenght - index, world.Rank + jumpToSecondProcessor, 1);
+                            for (int j = index; j < otherArrLenght; j++)
+                                world.Send<int>(otherArr[j], world.Rank + jumpToSecondProcessor, j - index + 2);
+                            otherArrLenght = index;
+                            otherArrNewLenght = world.Receive<int>(world.Rank + jumpToSecondProcessor, 1);
+                            for (int j = 0; j < otherArrNewLenght; j++)
+                                otherArr[otherArrLenght + j] = world.Receive<int>(world.Rank + jumpToSecondProcessor, j + 2);
+                            otherArrLenght += otherArrNewLenght;
+                            qs(ref otherArr, 0, otherArrLenght - 1);
                         }
                     }
                     world.Receive<int>(world.Rank - 1, 0);
+
                     var outputFile = new StreamWriter(@outputFileName, true);
                     outputFile.Write("world.Rank=" + world.Rank + ", b[]: ");
-                    for (int i = 0; i < bLenght; i++)
-                        outputFile.Write(b[i] + " ");
+                    for (int i = 0; i < otherArrLenght; i++)
+                        outputFile.Write(otherArr[i] + " ");
+
                     outputFile.Close();
-                    if (world.Rank != p - 1)
+
+                    if (world.Rank != proc - 1)
                         world.Send<int>(0, world.Rank + 1, 0);
                 }
             }
@@ -242,7 +251,7 @@ namespace MPIProgram
         /// </summary>
         /// <param name="p"></param>
         /// <returns>degree</returns>
-        static int DegreeTwo(int p)
+        static int considerDegreeTwo(int p)
         {
             if (testDegreeTwo(p))
             {
@@ -256,7 +265,7 @@ namespace MPIProgram
             }
             else
             {
-                Console.WriteLine("Не степень двойки");
+                Console.WriteLine("Не степень двойки.");
                 return -1;
             }
 
@@ -273,7 +282,7 @@ namespace MPIProgram
         {                                                               
             int ind=0;
             int first=0;
-            int last= bLength;// last=4;
+            int last= bLength; // last=4;
             while (first < last)
             {
                 ind = first + (last - first) / 2;
