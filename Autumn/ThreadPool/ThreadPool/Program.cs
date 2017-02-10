72ec558ff0534d57444403443c829b37200cf135
Nikita <nikit121a@mail.ru>
2017-02-11 00:43:35 +0300

    ThreadPool v1.0

---
+++ Autumn/ThreadPool/ThreadPool/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadPool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadPool pool = new ThreadPool();
+            for (int i = 0; i <= 5; i++)
+            {
+                string numAction = (i + 1).ToString();
+                pool.Enqueue(() =>
+                {
+                    Console.WriteLine("Action" + numAction + ": Work emulation");
+                    Thread.Sleep(500);
+                    Console.WriteLine("Action" + numAction + ": finish");
+                });
+            }
+            Console.WriteLine("Pool start");
+            pool.Start();
+            Console.WriteLine("If you want finish press something.");
+            Console.Read();
+            pool.Dispose();
+        }
+    }
+}
---
+++ Autumn/ThreadPool/ThreadPool/ThreadPool.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+namespace ThreadPool
+{
+    public class ThreadPool: IDisposable
+    {
+        private const int NumThread = 3;
+        private Queue<Action> taskQueue = new Queue<Action>();
+        private object threadLock = new object();
+        private Thread[] threadArr = new Thread[3];
+
+        public void Enqueue(Action task)
+        {
+            lock (threadLock)
+            {
+                taskQueue.Enqueue(task);
+            }
+        }
+
+        public void Dispose()
+        {
+            taskQueue.Clear();
+            Console.WriteLine("Dispose finished");
+        }
+
+        public void Start()
+        {
+            int _count = taskQueue.Count;
+            for (int j = 0; j <= _count; j++)
+            {
+                threadArr[j % 3] = new Thread(() =>
+                {
+                    if (taskQueue.Count != 0)
+                    {
+                        Action act;
+                        lock (threadLock)
+                        {
+                            act = taskQueue.Dequeue();
+                        }
+                        act();
+                    }
+                });
+                threadArr[j % 3].Start();
+            }
+        }
+
+
+
+
+    }
+}
