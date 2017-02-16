9f7aef21cd6b12b11f25e9db21e9087cd055fb1a
Nikita <nikit121a@mail.ru>
2017-02-16 08:14:32 +0300

    HTTP_Server test version.

---
+++ Autumn/HTTP_Server/HTTP_Server/Client.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace HTTP_Server
+{
+    
+}
---
+++ Autumn/HTTP_Server/HTTP_Server/Server.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTP_Server
+{
+    class Server
+    {
+        TcpListener Listener;
+        public Server(int port)
+        {
+            Listener = new TcpListener(IPAddress.Any, port);
+            Listener.Start();
+
+            while (true)
+            {
+                new Client(Listener.AcceptTcpClient());
+            }
+        }
+        class Client
+        {
+            public Client(TcpClient Client)
+            {
+                string html = "<html><body><h1>It work!</h1></body></html>";
+                string str = "HTTP/1.1 200 OK\nContent-type: text/html\nContent-Lenght: " + html.Length.ToString() + "\n\n" + html;
+
+                byte[] Buffer = Encoding.ASCII.GetBytes(str);
+
+                Client.GetStream().Write(Buffer, 0, Buffer.Length);
+                Client.Close();
+            }
+        }
+        static void Main(string[] args)
+        {
+            new Server(80);
+        }
+    }
+}
