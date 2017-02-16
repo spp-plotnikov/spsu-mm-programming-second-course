5f7518b8b94477b34989bf61256f00da37b5dc2b
Nikita <nikit121a@mail.ru>
2017-02-16 14:42:10 +0300

    HTTP_Server test version 2

--- Autumn/HTTP_Server/HTTP_Server/Server.cs
+++ Autumn/HTTP_Server/HTTP_Server/Server.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HTTP_Server
@@ -15,28 +16,29 @@ namespace HTTP_Server
         {
             Listener = new TcpListener(IPAddress.Any, port);
             Listener.Start();
-
             while (true)
             {
                 new Client(Listener.AcceptTcpClient());
             }
         }
-        class Client
-        {
-            public Client(TcpClient Client)
-            {
-                string html = "<html><body><h1>It work!</h1></body></html>";
-                string str = "HTTP/1.1 200 OK\nContent-type: text/html\nContent-Lenght: " + html.Length.ToString() + "\n\n" + html;
-
-                byte[] Buffer = Encoding.ASCII.GetBytes(str);
-
-                Client.GetStream().Write(Buffer, 0, Buffer.Length);
-                Client.Close();
-            }
-        }
+        
         static void Main(string[] args)
         {
             new Server(80);
+        }
+    }
+    class Client
+    {
+        public Client(TcpClient Client)
+        {
+            string html = "<html><body><h1>It work!</h1></body></html>";
+            string str = "HTTP/1.1 200 OK\nContent-type: text/html\nContent-Lenght: " + html.Length.ToString() + "\n\n" + html;
+
+            byte[] Buffer = Encoding.ASCII.GetBytes(str);
+
+            Client.GetStream().Write(Buffer, 0, Buffer.Length);
+            Thread.Sleep(10);
+            Client.Close();
         }
     }
 }
